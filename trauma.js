dd62025be198741662fdb20afcf6042c9ae155cb
Lining Wang <lining.wang@yale.edu>
2017-05-09 17:52:05 -0400

    placeholder art; gp

--- trauma.js
+++ trauma.js
@@ -1,23 +1,22 @@
 var mic, recorder, soundFile;
 var size, counter;
 var origSize;
+var img;
+var x, y;
 
 function setup() {
 	noCursor();
 	createCanvas(windowWidth, windowHeight);
 
-	mic = new p5.AudioIn();
-	mic.start();
-
-	recorder = new p5.SoundRecorder();
-	recorder.setInput(mic);
-	soundFile = new p5.SoundFile();
-
-	startArchive();
-
+	inputArchive();
+	// startArchive();
 	counter = 0;
-	origSize = 500;
+	origSize = windowWidth/2.5;
 	size = origSize;
+
+	img = loadImage("art1.jpg");
+	x = random(0, img.width);
+	y = random(0, img.height);
 }
 
 function draw() {
@@ -25,10 +24,17 @@ function draw() {
 	background(0);
 
 	var vol = mic.getLevel();
-	new Panel().display(vol);
-
-	// endDirection();
+	display(vol);
 	counter += 1;
+}
+
+function inputArchive() {
+	mic = new p5.AudioIn();
+	mic.start();
+
+	recorder = new p5.SoundRecorder();
+	recorder.setInput(mic);
+	soundFile = new p5.SoundFile();
 }
 
 function startArchive() {
@@ -52,26 +58,38 @@ function keyPressed() {
 	// completed();
 }
 
-function Panel (x, y) {
-	this.display = function(vol) {
-		var strokeColor = "red";
-		if (vol * 100 > 0.02 && !keyIsPressed) {
-			strokeColor = "white";
-			counter = 0;
-			frameRate(6);
-		}
-
-
-		size = max(0, (origSize - counter/3) + vol * 10 );
-		fill("white");
-		stroke(strokeColor);
-
-		var posX = windowWidth/2 - size/2;
-		var posY = windowHeight/2 - size/2;
-		for (var i = 0; i < 10; i++) {
-			var disp1 = random([-1, 0, 1]) * randomGaussian(vol * 1000);
-			var disp2 = random([-1, 0, 1]) * randomGaussian(vol * 1000);
-			rect(disp1 + posX, disp2 + posY, size, size);
-		}
+function display (vol) {
+	var strokeColor = "white";
+	if (vol * 100 > 0.01 && !keyIsPressed) {
+		strokeColor = "red";
+		counter = 0;
+		frameRate(10);
 	}
+
+	size = max(0, (origSize - counter/3) + vol * 300);
+	strokeWeight(0.2);
+	stroke(strokeColor);
+	fill("black");
+
+	var posX = windowWidth/2 - size/2;
+	var posY = windowHeight/2 - size/2;
+
+	for (var i = 0; i < 10; i++) {
+		var disp1 = random(-1, 1) * randomGaussian(vol * size);
+		var disp2 = random(-1, 1) * randomGaussian(vol * size);
+
+		x += disp1 * 5;
+		y += disp2 * 5;
+
+		print("x: " + x + "/" + img.width);
+		print("y: " + y + "/" + img.height);
+		rect(disp1 + posX, disp2 + posY, size, size);
+		image(img, disp1+posX, disp2+posY, size, size,
+			x, y, size, size);
+	}
+
+	if (x > img.width || x < 0)
+		x = random(0, img.width - size);
+	if (y > img.height || y < 0)
+		y = random(0, img.height - size);
 }
